29194c1bf8f2ae0bb0d799c791a3a9d792068aaf
Hadi Moshayedi <hadi@moshayedi.net>
2020-02-02 11:49:15 -0800

    Add curly braces

--- metapict-examples/common.rkt
+++ metapict-examples/common.rkt
@@ -37,3 +37,32 @@ latex
   (when (file-exists? filename)
     (delete-file filename))
   (save-pict filename p 'svg))
+
+(define (draw-rec cs)
+  (if (list? cs)
+      (draw* (map draw-rec cs))
+      (draw cs)))
+
+(define current-curly-brace-indent (make-parameter 0.5))
+(define (curly-brace p1 p2 [a (current-curly-brace-indent)])
+  (define (hcurve p1 va vb)
+    (curve p1 ..
+           (pt+ p1 (vec* 0.15 va) (vec* 0.5 vb)) ..
+           (pt+ p1 (vec* 0.6 va) (vec* 0.98 vb)) ..
+           (pt+ p1 va vb)))
+  (define v1 (pt- p2 p1))
+  (define vn (vec* (/ a (norm v1)) v1))
+  (define vp (rot90 vn))
+  (define vx (vec* 0.5 vn))
+  (define -vx (vec- (vec 0 0) vx))
+  (define vy (vec* 0.5 vp))
+  (define -vy (vec- (vec 0 0) vy))
+  (define p3 (pt+ (med 0.5 p1 p2) (vec* 2 vy)))
+  (curve-append
+   (hcurve p1 vx vy)
+   (curve (pt+ p1 vx vy) -- (pt+ p3 -vx -vy))
+   (curve-reverse (hcurve p3 -vx -vy))
+   (hcurve p3 vx -vy)
+   (curve (pt+ p3 (vec+ vx -vy)) -- (pt+ p2 -vx vy))
+   (curve-reverse (hcurve p2 -vx vy))))
+
