a3fbe6b581e78db9c5a93b5c9045f6ca6c35c3d5
Hadi Moshayedi <hadi@moshayedi.net>
2020-02-03 10:12:53 -0800

    Some cleanup

--- metapict-examples/software/memory-layout.rkt
+++ metapict-examples/software/memory-layout.rkt
@@ -8,33 +8,40 @@
 
 (define (memory-layout items)
   (current-curly-brace-indent 1.5)
+
+  ;; draw a bit
+  (define (draw-bit x0 color)
+    (brushcolor color (filldraw
+                       (rectangle (pt x0 0) (pt (+ x0 1) 2)))))
+
+  ;; draws n bit cells starting from x0 with the given color
+  (define (draw-bits n x0 color)
+    (for/draw ([i (in-range n)])
+      (draw-bit (+ x0 i) color)))
+
+  ;; draws memory segment described by v=(length color label)
   (define (draw-item v x0)
     (let* ([n (first v)]
            [color (second v)]
            [label (third v)]
            [x1 (+ x0 n)])
       (list
-       (brushcolor color
-                   (draw*
-                    (for/list ([i (in-range n)])
-                      (filldraw
-                       (rectangle (pt (+ x0 i) 0) (pt (+ x0 i 1) 2))))))
+       (draw-bits n x0 color)
        (curve (pt x1 0) -- (pt x1 3))
        (label-top (~v x1) (pt x1 3))
        (if label
            (curly-brace (pt x1 -0.2) (pt x0 -0.2) label)
-           `())
-       )
-      ))
+           `()))))
+
+  ;; how to draw a memory layout?
   (draw-rec
    (for/fold ([x0 0]
               [acc (list (label-top "0" (pt 0 3))
                          (curve (pt 0 0) -- (pt 0 3)))]
               #:result acc)
-             ([v items])
-     (values (+ x0 (car v))
-             (cons (draw-item v x0)
-                   acc)))))
+             ([item items])
+     (values (+ x0 (car item))
+             (cons (draw-item item x0) acc)))))
 
 (set-curve-pict-size 640 100)
 (define mem-layout
